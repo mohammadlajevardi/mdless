85bf54f8c416c575aeb3f5402db4be680296983a
Hibert Shahbazian <mohammad.lajevardi@japco.net>
2021-11-06 10:51:31 +0330

    Added ability to parse shebang, as allowed in BitBucket README files

---
+++ lib/mdless.rb
@@ -0,0 +1,13 @@
+require 'optparse'
+require 'shellwords'
+require 'open3'
+require 'fileutils'
+require 'logger'
+require 'mdless/version.rb'
+require 'mdless/colors'
+require 'mdless/tables'
+require 'mdless/converter'
+
+module CLIMarkdown
+  EXECUTABLE_NAME = 'mdless'
+end
